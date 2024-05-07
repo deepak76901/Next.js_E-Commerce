696a9efe5064ff5068137ce1fd0699ac32c6209c
deepak76901 <deepakb76901@gmail.com>
2024-05-07 15:21:29 +0530

    cart is protected now

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -1,15 +1,16 @@
 "use client";
 
+import Protected from "@/components/auth/Protected";
 import Cart from "@/components/Cart";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
 export default function page() {
   return (
-    <>
+    <Protected>
       <Navbar />
       <Cart />
       <Footer />
-    </>
+    </Protected>
   );
 }
